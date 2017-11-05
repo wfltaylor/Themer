06a55b3422b65e0bc5a339fd8a74c385275f7af8
wfltaylor <wfltaylor@gmail.com>
2017-11-05 16:41:52 +1100

    before work

---
+++ Themer/Classes/EasyUserInterface.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Themer
+//
+//  Created by William Taylor on 5/11/17.
+//  Copyright © 2017 William Taylor. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class themerEngine {
+    func setCurrentTheme
+}
---
+++ Themer/Themer.h
@@ -0,0 +1,19 @@
+//
+//  Themer.h
+//  Themer
+//
+//  Created by William Taylor on 5/11/17.
+//  Copyright © 2017 William Taylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Themer.
+FOUNDATION_EXPORT double ThemerVersionNumber;
+
+//! Project version string for Themer.
+FOUNDATION_EXPORT const unsigned char ThemerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Themer/PublicHeader.h>
+
+
